e37f00bc85de8dfbef58121e041035189b20135f
Berislav Mikulic <github.bm@gmail.com>
2022-03-26 22:04:50 +0100

    Few experimental changes
    Now possible to go through whole week of exercises
    Need to refactor and sort few things before merging
    with main.

--- crawlertockicanai.py
+++ crawlertockicanai.py
@@ -18,12 +18,21 @@ class CrawlerTockicanai:
         try:
             # exercise_hyperlink = self.__driver.find_element(By.XPATH, "//*[contains(text()," + exercise_name + " )]")
             # utils.highlight(exercise_hyperlink)
-            exercise_hyperlinks = WebDriverWait(self.__driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(text()," + exercise_name + " )]")))
+            # exercise_hyperlinks = WebDriverWait(self.__driver, 10).until(
+            # EC.presence_of_all_elements_located((By.XPATH, "//*[contains(text()," + exercise_name + " )]")))
+            sdasdas= 0
+            exercise_hyperlink = self.__driver.find_element(By.ID, "mec_skin_events_4147")
+            exercise_hyperlinks = exercise_hyperlink.find_elements(By.XPATH, "//*/div/div")
+
+            for i in range(1,6):
+                nesto = exercise_hyperlink.find_elements(By.XPATH, "//*/div/div/article["+i.__str__()+"]")
+                for bla in nesto:
+                    print(bla.text)
         finally:
             pass
 
-        for  hyperlinks in exercise_hyperlinks:
-            print(hyperlinks.rect)
+        # for  hyperlinks in exercise_hyperlinks:
+        #     print(hyperlinks.text)
 
 
     def book_exercise(self):
